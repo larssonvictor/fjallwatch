9db87acf7c8f1c681248665a7e31dae13ac87923
Victor Larsson <larsson.vic@gmail.com>
2016-07-26 21:04:21 +0200

    implemented the use of ejs partials and added a navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	res.render('index', { title: 'Fjallwatch' });
+});
+
+router.get('/webcamera', function(req, res, next) {
+	res.render('webcamera', {title: 'webcamera'});
 });
 
 module.exports = router;
